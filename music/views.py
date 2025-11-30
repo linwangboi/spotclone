ec0727c037d0920303646ea9bb92117e98212df3
linwangboi <linwangboi@gmail.com>
2025-11-30 11:16:15 +0000

    .

--- music/views.py
+++ music/views.py
@@ -64,6 +64,29 @@ def logout(request):
     auth.logout(request)
     return redirect('login')
 
+def get_audio_details(query):
+    url = "https://spotify-scraper.p.rapidapi.com/v1/track/download/soundcloud"
+
+    querystring = {"track": query,"quality":"sq"}
+
+    headers = {
+        "x-rapidapi-key": "c76c79dc85msh58c487ff509dc7ap1cdd5bjsn5f2523f93b17",
+        "x-rapidapi-host": "spotify-scraper.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    audio_details = []
+    if response.status_code == 200:
+        data = response.json()
+        track = data.get("soundcloudTrack")
+        if track and "audio" in track and track["audio"]:
+            first_audio = track["audio"][0]
+            audio_details.append(first_audio.get("url"))
+            audio_details.append(first_audio.get("durationText"))
+
+
+    return audio_details
+    
 
 @login_required(login_url='login')
 def music(request, pk):
@@ -79,8 +102,13 @@ def music(request, pk):
             track_name = data.get('name')
             artists = data.get('artists', [])
             first_artist_name = artists[0].get('name') if artists else 'Unknown'
+            audio_details = get_audio_details(track_name + first_artist_name)
+            audio_url = audio_details[0]
+            duration_text = audio_details[1]
             context = {
                 'track_name': track_name,
                 'artist_name': first_artist_name,
+                'audio_url': audio_url,
+                'duration_text': duration_text,
             }
     return render(request, 'music.html', context)
